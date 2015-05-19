522f214393dab20c3c6c016321123a94a72c136d
Gareth <gareth@entefy.com>
2015-05-19 16:09:51 -0700

    isWinningState

--- js/app.js
+++ js/app.js
@@ -43,10 +43,20 @@ angular.module('tictactoe', []).
 
         $scope.isWinningState = function() {
             for(var i = 0; i < 3; ++i) {
-                for(var k = 0; k < 3; ++k) {
-                    
+                if(getCell(i, 0) !== 0 && getCell(i, 0) === getCell(i, 1) && getCell(i, 1) === getCell(i, 2)) {
+                    return getCell(i, 0);
+                } else if(getCell(0, i) !== 0 && getCell(0, i) === getCell(1, i) && getCell(1, i) == getCell(2, i)) {
+                    return getCell(0, i);
                 }
             }
+
+            if(getCell(0,0) !== 0 && getCell(0,0) == getCell(1,1) && getCell(1,1) == getCell(2,2)) {
+                return getCell(0,0);
+            } else if(getCell(2, 0) !== 0 && getCell(2, 0) === getCell(1, 1) && getCell(1,1) == getCell(0, 2)) {
+                return getCell(2, 0);
+            }
+
+            return 0;
         }
 
     });
